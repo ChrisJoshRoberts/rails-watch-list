e0ecfd8424dfb889e1ec6bd5de1513148e1fc215
Chris Roberts <chrisrobertscollective@gmail.com>
2024-02-29 13:10:30 +0200

    adding seeds from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+response = URI.open(url).read
+movies = JSON.parse(response)
+
+movies["results"].each do |movie|
+  puts "making #{movie["original_title"]}"
+  Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+    )
+  puts "#{movie["original_title"]} created!"
+end
